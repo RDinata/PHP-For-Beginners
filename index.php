c97c0ba75ce47bbbf0551f23ac2b58bdcac7f4c2
Roby Dinata <demo@example.com>
2025-03-16 20:11:38 +0700

    08. Function and Filtering

--- index.php
+++ index.php
@@ -12,29 +12,50 @@
         [
             'name' => 'Do Androids Dream of Electric Sheep',
             'author' => 'Philip K. Dick',
+            'releaseYear' => 1968,
             'purchaseURL' => 'http://example.com'
         ],
         [
             'name' => 'The Langoliers',
             'author' => 'Stephen King',
+            'releaseYear' => 1990,
             'purchaseURL' => 'http://example.com'
         ],
         [
             'name' => 'Project Hail Mary',
             'author' => 'Andy Weir',
+            'releaseYear' => 2021,
+            'purchaseURL' => 'http://example.com'
+        ],
+        [
+            'name' => 'The Martian',
+            'author' => 'Andy Weir',
+            'releaseYear' => 2011,
             'purchaseURL' => 'http://example.com'
         ]
     ];
+
+    function filterByAuthor($books, $author)
+    {
+        $filteredBooks = [];
+
+        foreach ($books as $book) {
+            if ($book['author'] === $author) {
+                $filteredBooks[] = $book;
+            }
+        }
+        return $filteredBooks;
+    }
     ?>
 
     <ul>
-        <?php foreach ($books as $book) : ?>
+        <?php foreach (filterByAuthor($books, 'Andy Weir') as $book) : ?>
             <li>
                 <a href="<?= $book['purchaseURL'] ?>">
-                    <?= $book['name'] ?>
+                    <?= $book['name'] ?> (<?= $book['releaseYear'] ?>) - By <?= $book['author'] ?>
                 </a>
             </li>
-        <?php endforeach; ?>
+        <?php endforeach ?>
     </ul>
 </body>
 
