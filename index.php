fca8be7ea11871343b3b4b6cd780663be63d6b08
Roby Dinata <demo@example.com>
2025-03-17 21:46:58 +0700

    12. Page Links

--- index.php
+++ index.php
@@ -1,17 +1,3 @@
 <?php
 
-$business = [
-    'name' => 'Metal Gears Co.',
-    'cost' => 10000,
-    'categories' => ['industrial', 'widgets', 'sprockets']
-];
-
-function register($user)
-{
-    // create the user record in the db.
-    // log them in.
-    // send them a welcome email.
-    // redirect to their new dashboard.
-}
-
 require "index.view.php";
